451ac625385c0a44daae4bf4bfef2bf5b5ce9ee9
TimDer <timderksen97@icloud.com>
2020-01-26 18:09:17 +0100

    Remove database credentials

--- db.sample.php
+++ db.sample.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername	= "localhost";
-$username	= "root";
-$password	= "Admin";
-$dbname		= "xcms";
+$servername	= "";
+$username	= "";
+$password	= "";
+$dbname		= "";
 
 // create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
